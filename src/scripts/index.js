2d36fb09a99c7591571dedd499deea13ff9f8c1d
Naomi Hauret <hauretnaomi@gmail.com>
2021-06-15 21:40:58 +0200

    feat(header): remove alpine dependency with vanilla js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
-import 'alpinejs'
 import 'leaflet/dist/leaflet.css'
 import 'glider-js/glider.min.css'
 import lazyInit from '@helpers/lazyInit'
 import initializeMap from '@components/leaflet-map'
+import initializeMenu from '@components/menu'
 import Glider from 'glider-js/glider'
 
 // Initialize OpenStreetMap/Leaflet map when in viewport
@@ -10,6 +10,24 @@ const mapElement = document.querySelector('#clinic-map')
 lazyInit(mapElement, () => {
   initializeMap(mapElement)
 })
+
+// Menu
+const navbar = document.querySelector('[data-interactive="navbar"]')
+window.addEventListener('scroll', () => {
+    const scroll = document.documentElement.scrollTop
+    const classname = "has-shadow"
+    if (scroll > 20) {
+      navbar.classList.add(classname)
+    } else {
+      navbar.classList.remove(classname)
+    }
+  }
+)
+
+
+const menuControls = document.querySelectorAll('[data-interactive="menu-control"]')
+const menuTarget = document.querySelector('[data-interactive="menu-fold"]')
+initializeMenu(menuControls, menuTarget)
 
 new Glider(document.querySelector('#carousel'), {
   // Mobile-first defaults
